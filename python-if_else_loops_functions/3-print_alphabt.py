ccfe667351a02898a534b8b16d294c73e01895a7
Aydin-Akqubayev <aydinakqubayev1@gmail.com>
2025-11-16 20:45:06 +0400

    new-test

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for char in range(97, 123):
-    if char == ord('q')  or  char == ord('e') :
+    if char == ord('q') or char == ord('e'):
         continue
     else:
         print("{:c}".format(char), end="")
