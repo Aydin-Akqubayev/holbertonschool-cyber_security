8d799c6d68df2506817f9c64987778eafc046ec6
Aydin-Akqubayev <aydinakqubayev1@gmail.com>
2025-11-17 19:13:16 +0400

    question 2

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,46 +1,10 @@
 #!/usr/bin/python3
-def add(a, b):
+from calculator_1 import add, sub, mul, div
+a = 10
+b = 5
 
-    print(f"{a} + {b} = {a + b}")
-
-
-def sub(a, b):
-
-    print(f"{a} - {b} = {a - b}")
-
-
-def mul(a, b):
-    """My multiplication function
-
-    Args:
-        a: first integer
-        b: second integer
-
-    Returns:
-        The return value. a * b
-    """
-
-    print(f"{a} * {b} = {a * b}")
-
-
-def div(a, b):
-    """My division function
-
-    Args:
-        a: first integer
-        b: second integer
-
-    Returns:
-        The return value. a / b
-    """
-
-    print(f"{a} / {b} = {int(a / b)}")
-
-add(10, 5)
-
-
-sub(10, 5)
-
-mul(10, 5)
-
-div(10, 5)
+if __name__ == "__main__":
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
