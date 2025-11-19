36c37cab8f2c6d896f3ae2951980f979b47a810b
Aydin-Akqubayev <aydinakqubayev1@gmail.com>
2025-11-19 15:03:36 +0400

    new

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,8 +3,7 @@ def print_matrix_integer(matrix=""):
     result = ''
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
-            result+= "{} ".format(matrix[i][j])
-        result= result[:-1] + '\n'
+            result += "{:d} ".format(matrix[i][j])
+        result = result[:-1] + '\n'
 
     print(result[:-1])
-
